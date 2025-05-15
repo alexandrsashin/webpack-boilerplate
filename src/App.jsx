114397857284ecf7bf134cfea5b78ef0a2d367f4
alexandrsashin <alexandrsashin@gmail.com>
2025-05-15 13:35:38 +0300

    Changed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
 
-const App = () => (
-  <div className="app">
-    <h1>Hello, React + Webpack!</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello, React + Webpack!</h1>
+    </div>
+  );
+};
 
 export default App;
