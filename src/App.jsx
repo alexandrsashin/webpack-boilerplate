b938ddcefa90bf90a58d15777e199f1b79e51ada
alexandrsashin <alexandrsashin@gmail.com>
2025-05-15 13:33:34 +0300

    Added webpack project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.scss";
+
+const App = () => (
+  <div className="app">
+    <h1>Hello, React + Webpack!</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack/webpack.common.js
@@ -0,0 +1,44 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: path.resolve(__dirname, "../src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+    }),
+  ],
+};
+
+export default config;
